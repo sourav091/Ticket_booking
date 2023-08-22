c02e4f250385b37145e728f4e111c193a41cd102
sourav091 <mandalsourav99622@gmail.com>
2023-08-22 23:15:08 +0530

    Wrap ticketView widget on the  main screen Add image to container widget Wrap HotelScreen widget with a SingleChildScrollView widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/screens/hotel_screen.dart';
 import 'package:ecommerce_app/screens/ticket_view.dart';
+import 'package:ecommerce_app/utils/app_info_list.dart';
 import 'package:ecommerce_app/utils/app_styles.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
@@ -86,7 +88,40 @@ class HomeScreen extends StatelessWidget {
                 ],
               )),
           const Gap(20),
-         const  TicketView(),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: [
+                TicketView(),
+                TicketView(),
+              ],
+            ),
+         ),
+         const Gap(15),
+         Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+           child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Hotels",style: Styles.headLineStyle2,),
+                        InkWell(
+                            onTap: () {
+                              debugPrint("You are tapped");
+                            },
+                            child: Text("View all",style: Styles.textStyle.copyWith(color: Styles.primaryColor),))
+                      ],
+                    ),
+         ),
+         const Gap(15),
+         SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: hotelList.map((singleHotel) => HotelScreen(hotel: singleHotel)).toList()
+            )
+          ),
+
         ],
       ),
     );
---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,20 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.png',
+    'place': 'Open Space',
+    'destination': 'Mumbai',
+    'price': 45
+  },
+  {
+    'image': 'two.png',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 50
+  },
+  {
+    'image': 'three.png',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  },
+];
